9c467212891ac548711962f19763584d30e27982
Deyan Yanakiev <kliomba@abv.bg>
2014-07-01 16:08:13 +0300

    Functionality for select change

--- compare.js
+++ compare.js
@@ -3,16 +3,34 @@ $(document).ready(function() {
 
   var countries = [];
 
+
+
   $.getJSON('http://worldcup.sfg.io/teams/results', function(results) {
 
     results.forEach(function(item) {
       countries.push('<option value=\''+item.country+'\'>', item.country, '</option>');
     });
 
-    [0, 5].forEach(function(item) {
+    [0, 3].forEach(function(item) {
       generateCountryResults(results[item]);
     });
+
     recolorResults();
+
+    $('#compare-container').on('change', '.country-select', function() {
+      var country = $(this).val(),
+          countryResults;
+
+      results.forEach(function(item) {
+        if (item.country === country) {
+          countryResults = item;
+        }
+      });
+
+      changeCuntryResults(countryResults, $(this).parent());
+
+    });
+
   });
 
   function generateCountryResults(result) {
@@ -28,18 +46,20 @@ $(document).ready(function() {
 
   function recolorResults() {
     var $wins = $('span').filter('.wins'),
-        $loses = $('span').filter('.losses'),
+        $losses = $('span').filter('.losses'),
         $draws = $('span').filter('.draws'),
         $points = $('span').filter('.points');
 
     var maxWinsIndex = findMax($wins),
-        maxLosesIndex = findMax($loses),
-        maxDrawsIndex = findMax($draws),
+        minLossesIndex = findMin($losses),
+        minDrawsIndex = findMin($draws),
         maxPointsIndex = findMax($points);
 
+    $('.alert-success').removeClass('alert-success');
+    $('.alert-danger').removeClass('alert-danger');
     addColorClasses($wins, maxWinsIndex);
-    addColorClasses($loses, maxLosesIndex);
-    addColorClasses($draws, maxDrawsIndex);
+    addColorClasses($losses, minLossesIndex);
+    addColorClasses($draws, minDrawsIndex);
     addColorClasses($points, maxPointsIndex);
   }
 
@@ -57,14 +77,37 @@ $(document).ready(function() {
     return maxIndex;
   }
 
-  function addColorClasses($elements, maxIndex) {
+  function findMin($elements) {
+    var min = parseInt($elements.first().text(), 10),
+        minIndex = 0;
+
+    $elements.each(function(index, item) {
+      if (parseInt($(item).text(), 10) < min) {
+        min = parseInt($(item).text(), 10);
+        minIndex = index;
+      }
+    });
+
+    return minIndex;
+  }
+
+  function addColorClasses($elements, successIndex) {
     $elements.each(function(index, el) {
-      if (index === maxIndex) {
+      if (index === successIndex) {
         $(el).parent().addClass('alert-success');
       } else {
         $(el).parent().addClass('alert-danger');
       }
     });
+  }
+
+  function changeCuntryResults(results, $container) {
+    $container.find('span').filter('.wins').text(results.wins);
+    $container.find('span').filter('.losses').text(results.losses);
+    $container.find('span').filter('.draws').text(results.draws);
+    $container.find('span').filter('.points').text(results.points);
+
+    recolorResults();
   }
 
 });
